47a0da49a606953ac5a6ff61af88797881c5ecf5
6yhn <6yhn@fresnou.org>
2023-03-14 13:59:56 -0700

    initial comimit

---
+++ functionsnotes.js
@@ -0,0 +1,41 @@
+//Functions Notes
+// function sayHello(){
+//     console.log("hello")
+// }
+// sayHello();
+// sayHello();
+// sayHello();
+// sayHello();
+//  function printMax(){
+// //     let x = 6;
+// //     let y = 29;
+// //     if (x>y){
+// //         console.log(`the larger the number is ${x}`);
+// //     } else {
+// //         console.log(`the larger the number is ${y}`);
+// //     }
+// //  }
+// //
+// //  printMax();
+
+// function printMax(x, y){
+//     if (x>y){
+//         console.log(`the larger the number is ${x}`);
+//     }else {
+//         console.log('the larger the number is ${y');
+//     }
+// }
+//
+// printMax( x:8,y:21);
+
+printMax(7,13);
+printMax(7,-21);
+printMax(2,2);
+printMax(-8,21);
+
+function sum(number1, number2){
+    console.log(`${number1} + ${number2} = ${number1+number2}`);
+}
+
+sum(number1:7, number2:12);
+sum(number1:0, number2:);

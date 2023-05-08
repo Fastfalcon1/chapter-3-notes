11a76aec9b021da9777b3528a3967b658c8f6961
6yhn <6yhn@fresnou.org>
2023-05-08 14:05:39 -0700

    initial comimit

--- menu.js
+++ menu.js
@@ -1,4 +1,23 @@
 const readLine = require('readline-sync');
-    let word = [];
+let order = [];
+let cost = [];
 
-    console.log('hi what u want?');
+    order.push(  readLine.question("What sandwhich do you wanna order? We have a Chicken for $5.25, a beef for $6.25, and a tofu for $5.75! "));
+    if $(order[0]){
+
+    }
+        console.log(`You chose: ${order[0]}`);
+
+    let drink = [];
+
+    drink.push(readLine.question("Would you like a drink with this meal? We have small $1.00, medium $1.75, large $2.25!"));
+
+        console.log(`You got a ${drink[0]}`);
+
+        let flavor = [];
+        flavor.push(readLine.question("What would you like? we have Coke, Fanta, pepsi, gatorade, and RootBeer"));
+
+        console.log(`okay your total will be`);
+
+
+

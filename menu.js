61e1a6ff42d6bcd5d3299515c550cec9ba77d309
6yhn <6yhn@fresnou.org>
2023-05-15 14:11:09 -0700

    initial comimit

--- menu.js
+++ menu.js
@@ -1,23 +1,62 @@
-const readLine = require('readline-sync');
+const readLine = require ('readline-sync');
+
+let total = [];
+
 let order = [];
-let cost = [];
 
-    order.push(  readLine.question("What sandwhich do you wanna order? We have a Chicken for $5.25, a beef for $6.25, and a tofu for $5.75! "));
-    if $(order[0]){
+let sandwich = readLine.question("what sandwich you want? we got chicken $5.25, beef $6.25, and tofu for $5.75:")
 
+console.log(`you have chosen a ${sandwich}`);
+
+if (sandwich == "chicken"){
+    ordersandwich = 1
+    total.push(5.25)
+    order.push("chicken sandwich")
+}
+
+if (sandwich == "beef"){
+    ordersandwich = 1
+    total.push(6.25)
+    order.push("beef sandwich")
+}
+
+if (sandwich == "tofu"){
+    ordersandwich = 1
+    total.push(5.75)
+    order.push("tofu sandwich")
+}
+
+let drink = readLine.question("do you want a drink to quench your thirst?:")
+if(drink =="yes"){
+    let size = readLine.question("what size? a small is $1.00, a medium is $1.75, and a large is $2.25")
+    if (drink == "small"){
+        orderdrink = 1
+        total.push(1.00)
+        order.push("small drink")
     }
-        console.log(`You chose: ${order[0]}`);
 
-    let drink = [];
+    if (drink == "medium"){
+        orderdrink = 1
+        total.push(1.75)
+        order.push("medium drink")
+    }
 
-    drink.push(readLine.question("Would you like a drink with this meal? We have small $1.00, medium $1.75, large $2.25!"));
+    if (drink == "large"){
+        orderdrink = 1
+        total.push(2.25)
+        order.push("large drink")
+    }
+    console.log(`ok a ${sandwich} sandwich and a ${drink} drink.` )
+}
 
-        console.log(`You got a ${drink[0]}`);
+if (drink = "no"){
+    orderdrink = 1
+    console.log(`no? ok so a ${sandwich} sandwich` )
+}
 
-        let flavor = [];
-        flavor.push(readLine.question("What would you like? we have Coke, Fanta, pepsi, gatorade, and RootBeer"));
+let fries = readLine.question("do you want french fries?:")
 
-        console.log(`okay your total will be`);
-
-
-
+if (fries ="yes"){
+    let size =readLine.question("what size? small is $1.00, medium is $1.50, and a medium is $2.00")
+    console.log(`ok so a ${size} fry.` )
+}

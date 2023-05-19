f00d10a478b0478bf791219c026b0277c3802b38
6yhn <6yhn@fresnou.org>
2023-05-19 14:18:59 -0700

    initial comimit

--- menu.js
+++ menu.js
@@ -27,36 +27,139 @@ if (sandwich == "tofu"){
 }
 
 let drink = readLine.question("do you want a drink to quench your thirst?:")
-if(drink =="yes"){
+if (drink == "yes"){
     let size = readLine.question("what size? a small is $1.00, a medium is $1.75, and a large is $2.25")
-    if (drink == "small"){
+    if (size == "small"){
         orderdrink = 1
         total.push(1.00)
         order.push("small drink")
     }
 
-    if (drink == "medium"){
+    if (size == "medium"){
         orderdrink = 1
         total.push(1.75)
         order.push("medium drink")
     }
 
-    if (drink == "large"){
+    if (size == "large"){
         orderdrink = 1
         total.push(2.25)
         order.push("large drink")
     }
-    console.log(`ok a ${sandwich} sandwich and a ${drink} drink.` )
+    console.log(`ok a ${sandwich} sandwich and a ${size} drink.` )
 }
 
-if (drink = "no"){
+if (drink == "no"){
     orderdrink = 1
     console.log(`no? ok so a ${sandwich} sandwich` )
 }
 
 let fries = readLine.question("do you want french fries?:")
 
-if (fries ="yes"){
-    let size =readLine.question("what size? small is $1.00, medium is $1.50, and a medium is $2.00")
-    console.log(`ok so a ${size} fry.` )
+if (fries == "yes"){
+    let friesSize = readLine.question("what friesSize? small is $1.00, medium is $1.50, and a medium is $2.00")
+    if (friesSize == "small") {
+        let megasize = readLine.question("since you got small fries, do you wanna megasize your fries?:")
+        if (megasize == "yes") {
+            orderfries = 1
+            total.push(2.00)
+            order.push("mega size fries")
+        }
+        if(megasize =="no"){
+            orderfries = 1
+            console.log(`ok your loss`)
+        }
+    }
+    if (friesSize == "small"){
+        orderfires = 1
+        total.push(1.00)
+        order.push("small fries")
+    }
+    if (friesSize == "medium"){
+        orderfires = 1
+        total.push(1.50)
+        order.push("medium fries")
+    }
+    if (friesSize == "large"){
+        orderfires = 1
+        total.push(2.00)
+        order.push("large fries")
+    }
+    console.log(`so you will be having a ${friesSize} fry`)
 }
+
+if (fries == "no"){
+    orderfries = 1
+    console.log(`ok then...`)
+}
+
+let ketchupPacket = readLine.question("do you want any ketchup packets? they're on a special for $.25 each:")
+if (ketchupPacket == "yes"){
+    let many = readLine.question("how many?")
+    if (many == "1"){
+        orderketchupPacket= 1
+        total.push(0.25)
+        order.push("1 ketchupPacket")
+    }
+    if (many == "2"){
+        orderketchupPacket = 2
+        total.push(0.50)
+        order.push("2 ketchupPacket")
+    }
+    console.log(`ok ${many} Ketchup Packets` )
+}
+if (ketchupPacket == "no"){
+    orderketchupPacket = 1
+    console.log('ok then...')
+}
+let mustard = readLine.question(" would you like some mustard?:")
+if(mustard =="yes"){
+    let quantity = readLine.question("how many?:")
+    if (quantity == "1"){
+        ordermustard = 1
+        total.push(0.50)
+        order.push("1 mustard packet")
+    }
+    if (quantity == "2"){
+        ordermustard = 2
+        total.push(1.00)
+        order.push("2 mustard packets")
+    }
+    console.log(`ok so ${quantity} mustard packets`)
+}
+if (mustard =="no"){
+    ordermustard = 1
+    console.log('ummmm ok.....')
+}
+let hotsauce = readLine.question(" would you like some hotsauce?:")
+if(hotsauce =="yes"){
+    let amount = readLine.question("how many?:")
+    if (amount == "1"){
+        orderhotsauce = 1
+        total.push(0.50)
+        order.push("1 hotsauce packet")
+    }
+    if (amount == "2"){
+        orderhotsauce = 2
+        total.push(1.00)
+        order.push("2 hotsauce packets")
+    }
+    console.log(`ok so ${amount} hotsauce packets`)
+}
+if (hotsauce =="no"){
+    orderhotsauce = 1
+    console.log('ummmm ok.....')
+}
+let totalprice = 0;
+for(let i = 0; i < total.length; i++) {
+    totalprice += total[i]
+}
+if (ordersandwich + orderdrink + orderfries + orderketchupPacket + ordermustard + orderhotsauce === 6) {
+    total = totalprice - 1
+
+}
+totalprice = totalprice + (totalprice * 0.07);
+totalprice = totalprice.toFixed(2);
+
+console.log(`Your order is a ${order}`)
+console.log(`Your total is then, ${totalprice}!`)
